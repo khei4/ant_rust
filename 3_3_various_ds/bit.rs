b966b0e872f2ac3683bade14c6a00208ac58f8e4
KoheiAsano <kk.asano.luxy@gmail.com>
2020-04-29 10:38:32 +0900

    bit to usize

--- 3_3_various_ds/bit.rs
+++ 3_3_various_ds/bit.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct BIT<T> {
     table: Vec<T>,
 }
@@ -8,22 +9,22 @@ impl<T: std::default::Default + Clone + Copy + std::ops::AddAssign> BIT<T> {
         }
     }
     // 1..i番目の要素までの和を求める
-    fn sum(&self, mut i: i64) -> T {
+    fn sum(&self, mut i: usize) -> T {
         let mut sum: T = T::default();
         while i != 0 {
-            sum += self.table[i as usize];
-            i -= i & -i;
+            sum += self.table[i];
+            i &= i - 1;
         }
         sum
     }
 
-    fn add(&mut self, mut i: i64, x: T) {
+    fn add(&mut self, mut i: usize, x: T) {
         if i == 0 {
             panic!("0 is given to BIT");
         }
         while (i as usize) < self.table.len() {
             self.table[i as usize] += x;
-            i += i & -i;
+            i += i - (i & (i - 1));
         }
     }
 }
